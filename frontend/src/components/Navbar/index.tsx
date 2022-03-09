72a15a4037d5fcb295e5b0e2d4217d4ad5f78673
AlexsandroLealRicardo <alexsandro.ricardo@gmail.com>
2022-03-09 17:27:09 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './style.css'
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/AlexsandroLealRicardo"  >
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/AlexsandroLealRicardo</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+export default Navbar;
