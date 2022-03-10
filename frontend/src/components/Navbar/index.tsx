c110b42b248a550d9552fd26b3ceebb47a1f6d43
AlexsandroLealRicardo <alexsandro.ricardo@gmail.com>
2022-03-09 22:53:12 -0300

    MovieCard

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/AlexsandroLealRicardo"  >
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/AlexsandroLealRicardo -*----</p>
+                            <p className="dsmovie-contact-link">/AlexsandroLealRicardo</p>
                         </div>
                     </a>
                 </div>
